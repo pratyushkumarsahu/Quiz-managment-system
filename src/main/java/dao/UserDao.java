8a6d024f633ede731a23b4b5b8a46e335f239625
arpitajena20 <arpitajena1985@gmail.com>
2025-10-29 01:46:33 +0530

    commiting userdao

---
+++ src/main/java/dao/UserDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import dao.User;
+
+//@SuppressWarnings("unused")
+public interface UserDao {
+
+	boolean addUser(User user);
+	boolean isValidUser(String username, String password);
+	boolean isRecoveryCorrect(String username,String recovery);
+	User getUserByUsername(String username);
+	
+	
+}
